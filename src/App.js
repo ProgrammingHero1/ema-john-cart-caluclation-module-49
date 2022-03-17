57c63ec6837192c319edb065c2b041b90500ecc6
Programming Hero <programming.hero1@gmail.com>
2022-03-17 18:18:14 -0500

    added header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header></Header>
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import logo from '../../images/Logo.svg';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            <h2>This is Header</h2>
-        </div>
+        <nav className='header'>
+            <img src={logo} alt="" />
+            <div>
+                <a href="/shop">Shop</a>
+                <a href="/orders">Orders</a>
+                <a href="/inventory">Inventory</a>
+                <a href="/about">About</a>
+            </div>
+        </nav>
     );
 };
 
