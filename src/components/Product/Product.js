d3e00b57fdd59288e9b9743ae1c22af67c8c4f63
Programming Hero <programming.hero1@gmail.com>
2022-03-17 23:55:45 -0500

    syle the product component

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,11 +2,19 @@ import React from 'react';
 import './Product.css';
 
 const Product = (props) => {
-    const {name, img, seller, price, ratings} = props.product;
+    const { name, img, seller, price, ratings } = props.product;
     return (
         <div className='product'>
             <img src={img} alt=""></img>
-            <p className='product-name'>{name}</p>
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>Price: ${price}</p>
+                <p><small>Seller: {seller}</small></p>
+                <p><small>Ratings: {ratings} stars</small></p>
+            </div>
+            <button className='btn-cart'>
+                <p>Add to Cart</p>
+            </button>
         </div>
     );
 };
