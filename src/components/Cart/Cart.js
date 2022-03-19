b9cf1eea3c8c7ca13c4aa56836aa26aead40a543
Programming Hero <programming.hero1@gmail.com>
2022-03-19 08:54:42 -0500

    add product to the store

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -12,7 +12,8 @@ const Cart = (props) => {
         total = total + product.price;
         shipping = shipping + product.shipping;
     }
-    const tax = (total * 0.1).toFixed(2);
+    const tax = parseFloat((total * 0.1).toFixed(2));
+    const grandTotal = total + shipping + tax;
     return (
         <div className='cart'>
             <h4>Order Summary</h4>
@@ -20,7 +21,7 @@ const Cart = (props) => {
             <p>Total price: ${total}</p>
             <p>Total Shipping: ${shipping}</p>
             <p>Tax: {tax}</p>
-            <h5>Grand Total: </h5>
+            <h5>Grand Total: {grandTotal.toFixed(2)}</h5>
         </div>
     );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -18,6 +19,7 @@ const Shop = () => {
         // do not do this: cart.push(product);
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id);
     }
 
     return (
